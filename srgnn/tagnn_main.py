43a05924f5297b352e16ee68f15ea236bfdc8e1a
Kacper Puchalski <k.puchalski.xiv@gmail.com>
2024-06-28 11:26:42 +0200

    add tagnn code

---
+++ srgnn/tagnn_main.py
@@ -0,0 +1,425 @@
+#!/usr/bin/env python36
+# -*- coding: utf-8 -*-
+"""
+Created on July, 2018
+
+@author: Tangrizzly
+"""
+
+import argparse
+import os
+import pickle
+
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import yaml
+from pytorch_lightning.callbacks import (
+    EarlyStopping,
+    LearningRateMonitor,
+    ModelCheckpoint,
+)
+from sklearn.mixture import GaussianMixture
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from typing import List
+
+import wandb
+from srgnn_datasets import (
+    Augment_Matrix_Dataset,
+    Clusters_Matrix_Dataset,
+    SRGNN_Map_Dataset,
+    SRGNN_sampler,
+)
+from tagnn_model import TAGNN_model
+from utils import calculate_embeddings, fake_parser, split_validation
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--dataset",
+    default="sample",
+    help="dataset name: diginetica/yoochoose1_4/yoochoose1_64/sample",
+)
+parser.add_argument("--batchSize", type=int, default=100, help="input batch size")
+parser.add_argument("--hiddenSize", type=int, default=100, help="hidden state size")
+parser.add_argument(
+    "--epoch", type=int, default=30, help="the number of epochs to train for"
+)
+parser.add_argument(
+    "--lr", type=float, default=0.001, help="learning rate"
+)  # [0.001, 0.0005, 0.0001]
+parser.add_argument("--lr-dc", type=float, default=0.1, help="learning rate decay rate")
+parser.add_argument(
+    "--lr-dc-step",
+    type=int,
+    default=3,
+    help="the number of steps after which the learning rate decay",
+)
+parser.add_argument(
+    "--lr-milestones",
+    type=int,
+    default=[2,5, 8],
+    help="Shedule of steps after which the learning rate decay",
+    nargs='+'
+)
+parser.add_argument(
+    "--l2", type=float, default=1e-5, help="l2 penalty"
+)  # [0.001, 0.0005, 0.0001, 0.00005, 0.00001]
+parser.add_argument("--step", type=int, default=1, help="gnn propogation steps")
+parser.add_argument(
+    "--patience",
+    type=int,
+    default=6,
+    help="the number of epoch to wait before early stop ",
+)
+parser.add_argument(
+    "--nonhybrid", action="store_true", help="only use the global preference to predict"
+)
+parser.add_argument("--validation", action="store_true", help="validation")
+parser.add_argument(
+    "--valid_portion",
+    type=float,
+    default=0.1,
+    help="split the portion of training set as validation set",
+)
+parser.add_argument(
+    "--pretrained_embedings",
+    action="store_true",
+    help="initialize embeddings using word2vec",
+)
+parser.add_argument(
+    "--unfreeze_epoch",
+    type=int,
+    default=1,
+    help="epoch in which to unfreeze the embeddings layer",
+)
+parser.add_argument(
+    "--gmm", action="store_true", help="train GM on validation dataset after training"
+)
+parser.add_argument(
+    "--weight-init",
+    type=str,
+    default="normal",
+    help="Raw distances in adjacency matrix",
+)
+parser.add_argument(
+    "--augment-matrix",
+    action="store_true",
+    help="Use version of SRGNN with modified adjacency matrix",
+)
+parser.add_argument(
+    "--augment-clusters",
+    action="store_true",
+    help="Use clusters from GMM to modify adjacency matrix",
+)
+parser.add_argument(
+    "--augment-old-run-id",
+    type=str,
+    help="Full ID of an old run, to use embeddings from",
+)
+parser.add_argument(
+    "--augment-clip",
+    type=float,
+    default=0,
+    help="Max value at which to clip adjacency matrix",
+)
+parser.add_argument(
+    "--augment-normalize",
+    action="store_true",
+    help="Normalize adjacency matrix as in basic approach",
+)
+parser.add_argument(
+    "--augment-raw", action="store_true", help="Raw distances in adjacency matrix"
+)
+parser.add_argument(
+    "--augment-p",
+    type=float,
+    default=1.0,
+    help="Probability of matrix augmentation occuring",
+)
+parser.add_argument(
+    "--augment-mean",
+    type=float,
+    default=0.01,
+    help="Mean of gausian noise to inject into A",
+)
+parser.add_argument(
+    "--augment-std",
+    type=float,
+    default=0.0,
+    help="StandardDeviation of gausian noise to inject into A. Value equal to 0 corresponds to no noise injected",
+)
+parser.add_argument(
+    "--augment-categories",
+    action="store_true",
+    help="Use basic categories to modify adjacency matrix",
+)
+parser.add_argument(
+    "--augment-nogmm",
+    type=int,
+    default=16,
+    help="Number of gausoids used in GMM algorithm",
+)
+parser.add_argument(
+    "--augment-gmm-init",
+    type=str,
+    default='k-means++',
+    help="initialization of gausoids used in GMM algorithm",
+)
+parser.add_argument(
+    "--lr-scheduler",
+    type=str,
+    default="plateu",
+    help="Learning Rate scheduler to use",
+)
+
+opt = parser.parse_args()
+print(opt)
+
+
+def train_gm(model, dataset, dataloader):
+    session_emb = []
+
+    model.to("cuda")
+    for batch in tqdm(dataloader, total=dataset.length // opt.batchSize):
+        batch = [b.to("cuda") for b in batch]
+        session_emb.append(model.get_session_embeddings(batch).cpu().detach().numpy())
+    session_emb = np.concatenate(session_emb)
+
+    gm = GaussianMixture(n_components=32, n_init=2, init_params="k-means++")
+    _ = gm.fit_predict(session_emb)
+    with open(
+        f"./GMMs/gmm_val_{gm.n_components}_{gm.init_params}_{opt.hiddenSize}_{opt.dataset}_{opt.augment_matrix}.gmm",
+        "wb",
+    ) as gmm_file:
+        pickle.dump(gm, gmm_file)
+
+
+def main():
+    assert (
+        opt.lr_dc_step < opt.patience
+    ), "lr decrease patience is bigger or equal to early stopping patience. Please change either or both"
+
+    torch.set_float32_matmul_precision("medium")
+    train_data = pickle.load(open("../datasets/" + opt.dataset + "/train.txt", "rb"))
+    if opt.validation:
+        train_data, valid_data = split_validation(train_data, opt.valid_portion)
+    else:
+        test_data = pickle.load(open("../datasets/" + opt.dataset + "/test.txt", "rb"))
+
+    if opt.dataset == "amazon_cd":
+        n_node = 157661 + 1
+    elif opt.dataset == "amzaon_Baby_Products":
+        n_node = 89555 + 1
+    elif opt.dataset == "amzaon_Musical_Instruments":
+        n_node = 68465 + 1
+    elif opt.dataset == "diginetica":
+        n_node = 43098
+    elif opt.dataset == "yoochoose1_64" or opt.dataset == "yoochoose1_4":
+        n_node = 37484
+    elif opt.dataset == "yoochoose_nonspecial":
+        n_node = 37853 + 1
+    elif opt.dataset == "yoochoose_custom":
+        n_node = 28583
+    elif opt.dataset == "yoochoose_custom_augmented":
+        n_node = 27809
+    elif opt.dataset == "yoochoose_custom_augmented_5050":
+        n_node = 27807
+    else:
+        n_node = 310
+
+    embeddings = None
+    if opt.pretrained_embedings:
+        clicks_df = pickle.load(open(f"../datasets/{opt.dataset}/yoo_df.txt", "rb"))
+        items_in_train = pickle.load(
+            open(f"../datasets/{opt.dataset}/items_in_train.txt", "rb")
+        )
+        item2id = pickle.load(open(f"../datasets/{opt.dataset}/item2id.txt", "rb"))
+
+        embeddings = calculate_embeddings(
+            opt, clicks_df, items_in_train, item2id, n_node, epochs=10
+        )
+        print("embeddingas calculated")
+        del clicks_df
+        del items_in_train
+        del item2id
+
+    if opt.augment_matrix:
+        if opt.augment_clusters:
+            with open(
+                f"../datasets/{opt.dataset}/item_labels_{opt.augment_nogmm}_{opt.augment_gmm_init}_{opt.hiddenSize}_{opt.augment_old_run_id.split('-')[-1]}.txt",
+                "rb",
+            ) as f:
+                item_labels = pickle.load(f)
+            with open(
+                f"../datasets/{opt.dataset}/cluster_centers_{opt.augment_nogmm}_{opt.augment_gmm_init}_{opt.hiddenSize}_{opt.augment_old_run_id.split('-')[-1]}.txt",
+                "rb",
+            ) as f:
+                cluster_centers = pickle.load(f)
+
+            train_dataset = Clusters_Matrix_Dataset(
+                item_labels,
+                cluster_centers,
+                clip=opt.augment_clip,
+                normalize=opt.augment_normalize,
+                raw=opt.augment_raw,
+                p=opt.augment_p,
+                noise_mean=opt.augment_mean,
+                noise_std=opt.augment_std,
+                data=train_data,
+                shuffle=True,
+            )
+            del train_data
+
+            val_dataset = SRGNN_Map_Dataset(
+                #   item_labels,
+                #  cluster_centers,
+                # clip=opt.augment_clip,
+                # normalize=opt.augment_normalize,
+                # raw=opt.augment_raw,
+                data=valid_data
+            )
+            del valid_data
+        elif opt.augment_categories:
+            with open(
+                f"../datasets/{opt.dataset}/category_labels_{opt.hiddenSize}_{opt.augment_old_run_id.split('-')[-1]}.txt",
+                "rb",
+            ) as f:
+                item_labels = pickle.load(f)
+            with open(
+                f"../datasets/{opt.dataset}/category_embeddings_{opt.hiddenSize}_{opt.augment_old_run_id.split('-')[-1]}.txt",
+                "rb",
+            ) as f:
+                cluster_centers = pickle.load(f)
+
+            train_dataset = Clusters_Matrix_Dataset(
+                item_labels,
+                cluster_centers,
+                clip=opt.augment_clip,
+                normalize=opt.augment_normalize,
+                raw=opt.augment_raw,
+                p=opt.augment_p,
+                noise_mean=opt.augment_mean,
+                noise_std=opt.augment_std,
+                data=train_data,
+                shuffle=True,
+            )
+            del train_data
+            val_dataset = SRGNN_Map_Dataset(data=valid_data)
+            del valid_data
+
+        else:
+            with open(
+                f"./wandb/{opt.augment_old_run_id}/files/config.yaml", "r"
+            ) as stream:
+                config = yaml.safe_load(stream)
+
+            keys = list(config.keys())
+            for k in keys:
+                if k not in fake_parser().__dict__.keys():
+                    del config[k]
+                else:
+                    config[k] = config[k]["value"]
+
+            old_opt = fake_parser(**config)
+            assert (
+                old_opt.dataset == opt.dataset
+            ), f"Different datasets used in old ({old_opt.dataset}) and current ({opt.dataset}) models!"
+            assert (
+                old_opt.hiddenSize == opt.hiddenSize
+            ), f"Different hidden size used in old ({old_opt.hiddenSize}) and current ({opt.hiddenSize}) models!"
+
+            emb_model = TAGNN_model.load_from_checkpoint(
+                f"./GNN_master/{opt.augment_old_run_id.split('-')[-1]}/checkpoints/"
+                + os.listdir(
+                    f"./GNN_master/{opt.augment_old_run_id.split('-')[-1]}/checkpoints/"
+                )[0],
+                opt=old_opt,
+            ).model.embedding
+            train_dataset = Augment_Matrix_Dataset(
+                emb_model,
+                clip=opt.augment_clip,
+                normalize=opt.augment_normalize,
+                raw=opt.augment_raw,
+                p=opt.augment_p,
+                noise_mean=opt.augment_mean,
+                noise_std=opt.augment_std,
+                data=train_data,
+                shuffle=True,
+            )
+            del train_data
+            val_dataset = SRGNN_Map_Dataset(
+                # emb_model,
+                # clip=opt.augment_clip,
+                # normalize=opt.augment_normalize,
+                # raw=opt.augment_raw,
+                data=valid_data
+            )
+            del valid_data
+    else:
+        train_dataset = SRGNN_Map_Dataset(
+            train_data,
+            shuffle=True,
+            p=opt.augment_p,
+            noise_mean=opt.augment_mean,
+            noise_std=opt.augment_std,
+        )
+        del train_data
+        val_dataset = SRGNN_Map_Dataset(valid_data)
+        del valid_data
+
+    train_dataloader = DataLoader(
+        train_dataset,
+        num_workers=os.cpu_count() - 2,
+        sampler=SRGNN_sampler(
+            train_dataset, opt.batchSize, shuffle=True, drop_last=False
+        ),
+    )
+    val_dataloader = DataLoader(
+        val_dataset,
+        num_workers=os.cpu_count() - 2,
+        sampler=SRGNN_sampler(
+            val_dataset, opt.batchSize, shuffle=False, drop_last=False
+        ),
+    )
+
+    model = TAGNN_model(opt, n_node, init_embeddings=embeddings, **(opt.__dict__))
+    wandb_logger = pl.loggers.WandbLogger(
+        project="GNN_master", entity="kpuchalskixiv", log_model=True
+    )
+
+    trainer = pl.Trainer(
+        max_epochs=60,
+        limit_train_batches=train_dataset.length // opt.batchSize,
+        limit_val_batches=val_dataset.length // opt.batchSize,
+        callbacks=[
+            EarlyStopping(
+                monitor="val_hit", patience=opt.patience, mode="max", check_finite=True
+            ),
+            LearningRateMonitor(),
+            ModelCheckpoint(monitor="val_loss", mode="min"),
+        ],
+        logger=wandb_logger,
+    )
+    trainer.fit(
+        model=model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader
+    )
+    run_id = wandb.run.id
+    wandb.define_metric("val_loss", summary="min")
+    wandb.define_metric("val_hit", summary="max")
+    wandb.define_metric("val_mrr", summary="max")
+    print("Finished training. Run id: ", run_id)
+    wandb.finish()
+    if opt.gmm:
+        del train_dataset
+        del train_dataloader
+        model = TAGNN_model.load_from_checkpoint(
+            f"./GNN_master/{run_id}/checkpoints/"
+            + os.listdir(f"./GNN_master/{run_id}/checkpoints/")[0],
+            opt=opt,
+        )
+        train_gm(model, val_dataset, val_dataloader)
+
+
+if __name__ == "__main__":
+    main()
---
+++ srgnn/tagnn_model.py
@@ -0,0 +1,295 @@
+#!/usr/bin/env python38
+# -*- coding: utf-8 -*-
+"""
+Created on July, 2018
+
+@author: Tangrizzly
+"""
+
+import math
+
+import pytorch_lightning as pl
+import torch
+import torch.nn.functional as F
+from pytorch_lightning.utilities.types import STEP_OUTPUT
+from torch import nn
+from torch.nn import Module, Parameter
+
+
+class GNN(Module):
+    def __init__(self, hidden_size, step=1):
+        super(GNN, self).__init__()
+        self.step = step
+        self.hidden_size = hidden_size
+        self.input_size = hidden_size * 2
+        self.gate_size = 3 * hidden_size
+        self.w_ih = Parameter(torch.Tensor(self.gate_size, self.input_size))
+        self.w_hh = Parameter(torch.Tensor(self.gate_size, self.hidden_size))
+        self.b_ih = Parameter(torch.Tensor(self.gate_size))
+        self.b_hh = Parameter(torch.Tensor(self.gate_size))
+        self.b_iah = Parameter(torch.Tensor(self.hidden_size))
+        self.b_oah = Parameter(torch.Tensor(self.hidden_size))
+
+        self.linear_edge_in = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+        self.linear_edge_out = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+        self.linear_edge_f = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+
+    def GNNCell(self, A, hidden):
+        input_in = (
+            torch.matmul(A[:, :, : A.shape[1]], self.linear_edge_in(hidden))
+            + self.b_iah
+        )
+        input_out = (
+            torch.matmul(
+                A[:, :, A.shape[1] : 2 * A.shape[1]], self.linear_edge_out(hidden)
+            )
+            + self.b_oah
+        )
+        inputs = torch.cat([input_in, input_out], 2)
+        gi = F.linear(inputs, self.w_ih, self.b_ih)
+        gh = F.linear(hidden, self.w_hh, self.b_hh)
+        i_r, i_i, i_n = gi.chunk(3, 2)
+        h_r, h_i, h_n = gh.chunk(3, 2)
+        resetgate = torch.sigmoid(i_r + h_r)
+        inputgate = torch.sigmoid(i_i + h_i)
+        newgate = torch.tanh(i_n + resetgate * h_n)
+        hy = newgate + inputgate * (hidden - newgate)
+        return hy
+
+    def forward(self, A, hidden):
+        for i in range(self.step):
+            hidden = self.GNNCell(A, hidden)
+        return hidden
+
+
+class TagnnSessionGraph(Module):
+    def __init__(self, opt, n_node):
+        super(TagnnSessionGraph, self).__init__()
+        self.hidden_size = opt.hiddenSize
+        self.n_node = n_node
+        self.batch_size = opt.batchSize
+        self.nonhybrid = opt.nonhybrid
+        self.embedding = nn.Embedding(self.n_node, self.hidden_size)
+        self.gnn = GNN(self.hidden_size, step=opt.step)
+        self.linear_one = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+        self.linear_two = nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+        self.linear_three = nn.Linear(self.hidden_size, 1, bias=False)
+        self.linear_transform = nn.Linear(self.hidden_size * 2, self.hidden_size, bias=True)
+        self.linear_t = nn.Linear(self.hidden_size, self.hidden_size, bias=False)  #target attention
+        self.loss_function = nn.CrossEntropyLoss()
+      #  self.optimizer = torch.optim.Adam(self.parameters(), lr=opt.lr, weight_decay=opt.l2)
+      #  self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=opt.lr_dc_step, gamma=opt.lr_dc)
+        self.reset_parameters(opt.weight_init)
+
+    def reset_parameters(self, weight_init):
+        if weight_init == "uniform":
+            stdv = 1.0 / math.sqrt(self.hidden_size)
+            for weight in self.parameters():
+                nn.init.uniform_(weight, -stdv, stdv)
+        elif weight_init == "normal":
+            for weight in self.parameters():
+                nn.init.normal_(weight, 0, 0.1)
+        elif weight_init == "xavier_normal":
+            for weight in self.parameters():
+                if len(weight.shape) < 2:
+                    nn.init.normal_(weight, 0, 0.1)
+                else:
+                    nn.init.xavier_normal_(weight)
+        else:
+            raise ValueError(
+                f"Weight initialization of type {weight_init} not implemented!"
+            )
+
+    def compute_scores(self, hidden, mask):
+        ht = hidden[torch.arange(mask.shape[0]).long(), torch.sum(mask, 1) - 1]  # batch_size x latent_size
+        q1 = self.linear_one(ht).view(ht.shape[0], 1, ht.shape[1])  # batch_size x 1 x latent_size
+        q2 = self.linear_two(hidden)  # batch_size x seq_length x latent_size
+        alpha = self.linear_three(torch.sigmoid(q1 + q2))  # (b,s,1)
+        # alpha = torch.sigmoid(alpha) # B,S,1
+        alpha = F.softmax(alpha, 1) # B,S,1
+        a = torch.sum(alpha * hidden * mask.view(mask.shape[0], -1, 1).float(), 1)  # (b,d)
+        if not self.nonhybrid:
+            a = self.linear_transform(torch.cat([a, ht], 1))
+        b = self.embedding.weight[1:]  # n_nodes x latent_size
+        # target attention: sigmoid(hidden M b)
+        # mask  # batch_size x seq_length
+        hidden = hidden * mask.view(mask.shape[0], -1, 1).float()  # batch_size x seq_length x latent_size
+        qt = self.linear_t(hidden)  # batch_size x seq_length x latent_size
+        # beta = torch.sigmoid(b @ qt.transpose(1,2))  # batch_size x n_nodes x seq_length
+        beta = F.softmax(b @ qt.transpose(1,2), -1)  # batch_size x n_nodes x seq_length
+        target = beta @ hidden  # batch_size x n_nodes x latent_size
+        a = a.view(ht.shape[0], 1, ht.shape[1])  # b,1,d
+        a = a + target  # b,n,d
+        scores = torch.sum(a.to(torch.float16) * b.to(torch.float16), -1).to(torch.float32)  # b,n
+        # scores = torch.matmul(a, b.transpose(1, 0))
+        return scores
+
+    def session_embedding(self, hidden, mask):
+        ht = hidden[
+            torch.arange(mask.shape[0]).long(), torch.sum(mask, 1) - 1
+        ]  # batch_size x latent_size
+
+        q1 = self.linear_one(ht).view(
+            ht.shape[0], 1, ht.shape[1]
+        )  # batch_size x 1 x latent_size
+        q2 = self.linear_two(hidden)  # batch_size x seq_length x latent_size
+        alpha = self.linear_three(torch.sigmoid(q1 + q2))
+        a = torch.sum(alpha * hidden * mask.view(mask.shape[0], -1, 1).float(), 1)
+
+        if not self.nonhybrid:
+            a = self.linear_transform(torch.cat([a, ht], 1))
+        return a
+
+    def forward(self, inputs, A):
+        hidden = self.embedding(inputs)
+        hidden = self.gnn(A, hidden)
+        return hidden
+
+
+
+class TAGNN_model(pl.LightningModule):
+    def __init__(self, opt=None, n_node=0, init_embeddings=None, **kwargs):
+        super().__init__()
+        self.lr = opt.lr
+        self.save_hyperparameters(ignore=["opt", "init_embeddings"])
+        self.save_hyperparameters({'name': 'TAGNN'})
+        self.model = TagnnSessionGraph(opt, n_node)
+        if init_embeddings is not None:
+            self.model.embedding = nn.Embedding.from_pretrained(
+                torch.FloatTensor(init_embeddings)
+            )
+        self.unfreeze_epoch = opt.unfreeze_epoch
+
+    def forward(self, x):
+        for i in range(len(x)):
+            x[i] = x[i].squeeze(dim=0)
+
+        alias_inputs, A, items, mask = x
+        items = items.to(torch.int32)
+        A = A.to(torch.float32)
+        hidden = self.model(items, A)
+        get = lambda i: hidden[i][alias_inputs[i]]
+        seq_hidden = torch.stack(
+            [get(i) for i in torch.arange(len(alias_inputs)).long()]
+        )
+        return self.model.compute_scores(seq_hidden, mask)
+
+    def training_step(self, batch, batch_idx):
+        x = batch[:-1]
+        targets = batch[-1].squeeze()
+        scores = self.forward(x)
+
+        loss = self.model.loss_function(scores, targets - 1)
+        self.log("train_loss", loss, prog_bar=True)
+
+        return loss
+
+    def evaluate(self, batch, stage=None):
+        x = batch[:-1]
+        targets = batch[-1].squeeze()
+        scores = self.forward(x)
+        loss = self.model.loss_function(scores, targets - 1)
+
+        # get metrics @20
+        # hit is recall/precision, that is
+        # proportion of cases having the desired item amongst the top-20 items
+        sub_scores = scores.topk(20)[1]
+        hit, mrr = [], []
+        for score, target in zip(sub_scores, targets):
+            correct_pred = torch.isin(target - 1, score)
+            hit.append(correct_pred)
+            if not correct_pred:
+                mrr.append(0)
+            else:
+                mrr.append(1 / (torch.where(score == target - 1)[0][0] + 1))
+        hit = 100 * sum(hit) / targets.shape[0]
+        mrr = 100 * sum(mrr) / targets.shape[0]
+        if stage:
+            self.log(stage + "_loss", loss, prog_bar=True)
+            self.log(stage + "_hit", hit, prog_bar=True)
+            self.log(stage + "_mrr", mrr, prog_bar=True)
+
+    def validation_step(self, batch, *args, **kwargs):
+        return self.evaluate(batch, "val")
+
+    def test_step(self, batch, *args, **kwargs):
+        return self.evaluate(batch, "test")
+
+    def predict_step(self, batch, *args, **kwargs):
+        x = batch[:-1]
+        targets = batch[-1]
+        scores = self.forward(x)
+        sub_scores = scores.topk(20)[1]
+        return sub_scores, targets
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(
+            self.parameters(), lr=self.lr, weight_decay=self.hparams.l2
+        )
+        if self.hparams.lr_scheduler == "plateu":
+            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                optimizer,
+                patience=self.hparams.lr_dc_step,
+                factor=self.hparams.lr_dc,
+                cooldown=1,
+            )
+        elif self.hparams.lr_scheduler == "step":
+            scheduler = torch.optim.lr_scheduler.StepLR(
+                optimizer,
+                step_size=self.hparams.lr_dc_step,
+                gamma=self.hparams.lr_dc,
+            )
+        elif self.hparams.lr_scheduler == "multistep":
+            scheduler = torch.optim.lr_scheduler.MultiStepLR(
+                optimizer,
+                milestones=self.hparams.lr_milestones,
+                gamma=self.hparams.lr_dc,
+            )
+        else:
+            raise ValueError('Unknown (or not implemented) learning rate sheduler')
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "monitor": "val_loss",
+                "interval": "epoch",
+                "frequency": 1,
+                "name": "scheduler_lr",
+            },
+        }
+
+    def get_raw_embeddings(self, batch):
+        items = batch[2]
+        embs = self.model.embedding(items)
+        return embs
+
+    def get_session_embeddings(self, batch):
+        x = batch[:-1]
+        for i in range(len(x)):
+            x[i] = x[i].squeeze()
+        alias_inputs, A, items, mask = x
+        items = items.to(torch.int32)
+        A = A.to(torch.float32)
+        hidden = self.model(items, A)
+        get = lambda i: hidden[i][alias_inputs[i]]
+        seq_hidden = torch.stack(
+            [get(i) for i in torch.arange(len(alias_inputs)).long()]
+        )
+        return self.model.session_embedding(seq_hidden, mask)
+
+    def get_gnn_embeddings(self, batch):
+        x = batch[:-1]
+        _, A, items, _ = x
+        A = A.to(torch.float32)
+        hidden = self.model(items, A)
+        return hidden
+
+    def unfreeze_embeddings(self):
+        self.model.embedding.requires_grad_(True)
+
+    def freeze_embeddings(self):
+        self.model.embedding.requires_grad_(False)
+
+    def on_train_epoch_end(self):
+        if self.current_epoch == self.unfreeze_epoch:
+            self.unfreeze_embeddings()
